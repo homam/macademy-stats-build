7103b29113af03f98dc97206c74a5c2670d298d9
Homam <bluesnowball@gmail.com>
2013-09-19 01:22:48 +0400

    auto build

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node app.js
+web: NODE_ENV=production node_modules/coffee-script/bin/coffee app.coffee
---
+++ build-heroku.sh
@@ -0,0 +1,3 @@
+cp -rf . ../../macademy-stats-build/
+cd ../../macademy-stats-build
+rm -rf d3-template/.git
